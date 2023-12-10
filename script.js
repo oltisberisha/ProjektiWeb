0fd3c9cef479c12c4389a8357906031442b8c10b
unknown <ob64900@ubt-uni.net>
2023-12-10 16:32:06 +0100

    validimiupdate

--- script.js
+++ script.js
@@ -13,17 +13,15 @@ loginLink.addEventListener('click', () => {
 loginForm.addEventListener('submit', (event) => { 
   event.preventDefault(); 
 
-  window.location.href = "Home.html"; 
 });
 function validateForm() {
   var username = document.getElementById("Username").value;
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
-  var termsCheckbox = document.querySelector('input[type="checkbox"]');
 
-
-  if (username === "" || email === "" || password === "") {
-      alert("All fields must be filled out");
+  var usernameRegex = /^[A-z]+[a-zA-Z]+$/;
+  if (!usernameRegex.test(username)) {
+      alert("Please enter a valid username");
       return false;
   }
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -31,7 +29,31 @@ function validateForm() {
       alert("Please enter a valid email address");
       return false;
   }
+  var passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+  if (!passwordRegex.test(password)) {
+      alert("Please enter a valid password");
+      return false;
+  }
   alert("Registration successful!");
   wrapper.classList.remove('active');
+  return true;
+}
+function validateLoginForm() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address");
+      return false;
+  }
+
+  var passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+  if (!passwordRegex.test(password)) {
+      alert("Please enter a valid password");
+      return false;
+  }
+
+  alert("Login successful!");
   return true;
 }
